5af764b46d1db1d46ecad9bc7733165a7115db6a
eynol <625608852@qq.com>
2024-11-16 23:48:33 +0800

    init flakes

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "a beginner's flakes config";
+
+ # the nixConfig here only affects the flake itself, not the system configuration!
+  nixConfig = {
+    # override the default substituters
+    substituters = [
+      # cache mirror located in China
+      # status: https://mirror.sjtu.edu.cn/
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
+      # status: https://mirrors.ustc.edu.cn/status/
+      # "https://mirrors.ustc.edu.cn/nix-channels/store"
+
+      "https://cache.nixos.org"
+
+      # nix community's cache server
+      "https://nix-community.cachix.org"
+    ];
+#    trusted-public-keys = [
+#      # nix community's cache server public key
+#      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+#    ];
+  };
+
+  # Work-in-progress: refer to parent/sibling flakes in the same repository
+  # inputs.c-hello.url = "path:../c-hello";
+
+  outputs = { self, nixpkgs, ... }: {
+
+    # 因此请将下面的 my-nixos 替换成你的主机名称
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # 这里导入之前我们使用的 configuration.nix，
+        # 这样旧的配置文件仍然能生效
+	./hardware-configuration.nix
+        ./configuration.nix
+ 	{
+            # given the users in this list the right to specify additional substituters via:
+            #    1. `nixConfig.substituters` in `flake.nix`
+            nix.settings.trusted-users = [ "eynol" ];
+          }
+      ];
+    };
+  };
+}
